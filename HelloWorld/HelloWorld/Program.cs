c3286a173438cc20a2f14ab1526d3b314cae3954
jamess <jamessymon@DESKTOP-A50DHPO.localdomain>
2022-02-06 16:16:03 +0000

    Console Project

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+// See https://aka.ms/new-console-template for more information
+
